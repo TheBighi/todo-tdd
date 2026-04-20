7989c2bfa3c3d0669c54fbd91c4c883e95c22fd7
TheBighi <thebighix@gmail.com>
2026-04-20 11:37:56 +0300

    Create TodoController unit test wit http mocks

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,9 @@
-const createTodo = (req, res) => {
+const TodoModel = require('../models/todo.model');
 
-}
+const createTodo = (req, res) => {
+    const createdModel = TodoModel.create(req.body);
+    res.status(201).json(createdModel);
+};
 
 module.exports = {
     createTodo
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,32 @@
 const TodoController = require('../../controllers/todo.controller');
+const TodoModel = require('../../models/todo.model');
+const httpMocks = require('node-mocks-http');
+const newTodo = require('../mock-data/new-todo.json');
+
+TodoModel.create = jest.fn();
+
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+});
 
 describe('TodoController.createTodo', () => {
+    beforeEach(() => {
+        req.body = newTodo;
+    })
     it('should create a new todo item', () => {
         expect(typeof TodoController.createTodo).toBe('function');
+    })
+    it('should call TodoMode.create', () => {
+        TodoController.createTodo(req, res, next);
+        expect(TodoModel.create).toHaveBeenCalledWith(newTodo);
+    })
+    it('it should return 201', () => {
+        TodoModel.create.mockReturnValue(newTodo);
+        TodoController.createTodo(req, res, next);
+        expect(res._getJSONData()).toStrictEqual(newTodo);
     })
 })
